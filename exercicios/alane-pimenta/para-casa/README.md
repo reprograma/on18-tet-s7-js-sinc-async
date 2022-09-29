22cec44abc83f70b295eaacf320218a9397f6190
Alaneide Pimenta <107659459+Alanep0922@users.noreply.github.com>
2022-09-29 10:59:54 -0300

    Resolução da atividade de casa

--- exercicios/alane-pimenta/para-casa/README.md
+++ exercicios/alane-pimenta/para-casa/README.md
@@ -11,7 +11,7 @@ dados:
 
 Terminou o exercício? Dá uma olhada nessa checklist e confere se tá tudo certinho, combinado?!
 
-- [ ] Fiz o fork do repositório.
+- [ ] Fiz o fork do repositório. 
 - [ ] Clonei o fork na minha máquina (`git clone url-do-meu-fork`).
 - [ ] Resolvi o exercício.
 - [ ] Adicionei as mudanças. (`git add .` para adicionar todos os arquivos, ou `git add nome_do_arquivo` para adicionar um arquivo específico)
---
+++ exercicios/alane-pimenta/para-casa/index.js
@@ -0,0 +1,37 @@
+function buscarPrecoDolar() {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve({
+        comercial: "5.26",
+        turismo: "5.50",
+      });
+    }, 1000);
+  });
+}
+
+function buscarJurosImportacao() {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve({
+        juros1: 0.13,
+        juros2: 0.07,
+        message:
+        "os dois juros são aplicados no valor total do produto em real",
+      });
+    }, 1000);
+  });
+}
+
+async function calcularValorEmReal(precoEmDolar) {
+  try {
+    const precoDolar = await buscarPrecoDolar()
+    const jurosImportacao = await buscarJurosImportacao ()
+    const valorEmReal = precoEmDolar * precoDolar.comercial
+    const precoFinal = valorEmReal + (valorEmReal * jurosImportacao.juros1) +  (valorEmReal * jurosImportacao.juros2)
+    console.log(`Ò preço final do produto é R${precoFinal.toFixed(2).replace('.', ',')} reais`)
+  } 
+  catch (error) {
+     console.error(error)
+  }
+}
+calcularValorEmReal(1270);
