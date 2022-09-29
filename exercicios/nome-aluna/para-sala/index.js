a4c00025459ca157c85e8cf733c28808bc0f8600
Lilit Bandeira <lili@Lilits-Macbook-Air.local>
2022-09-28 22:05:40 -0300

    terceiro commit da professora - conteudo dado na revisão do dia 28.09.22

--- exercicios/nome-aluna/para-sala/index.js
+++ exercicios/nome-aluna/para-sala/index.js
@@ -1,33 +1,49 @@
 // função que simula busca num banco que retorna o preço do produto:
 
 function buscarPreco(produto) {
-  setTimeout(() => {
-    if (produto === "hormonios") {
-      return {
-        nome: "Hormônios",
-        preco: 99.00
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (produto === "hormonios") {
+        return resolve({
+          nome: "Hormônios",
+          preco: 99.00
+        });
+      } else if (produto === "unhas gel") {
+        return resolve({
+          nome: "Unhas em Gel",
+          preco: 190.00
+        });
+      } else if (produto === "lace"){
+        return resolve({
+          nome: "Lace",
+          preco: 3900.00
+        });
+      } else {
+        return reject("Produto não encontrado");
       }
-    } else if (produto === "unhas gel") {
-      return {
-        nome: "Unhas em Gel",
-        preco: 190.00
-      }
-    } else if (produto === "lace"){
-      return {
-        nome: "Lace",
-        preco: 3900.00
-      }
-    } else {
-      return "Produto não encontrado"
-    }
-  }, 2000)
+    }, 2000)
+  })
 }
 
 // função que simula busca num banco que retorna o valor das parcelas:
 
-function calcularParcela(preco) {
-  let parcelasDesejadas = 10
-  setTimeout(() => {
-    return preco * parcelasDesejadas
-  }, 2000)
+function calcularParcela(preco, numeroParcelas) {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      return resolve(preco / numeroParcelas);
+    }, 2000)
+  })
 }
+
+async function resolver(produto, numeroParcelas) {
+  try {
+    const dadosDoProduto = await buscarPreco(produto); 
+    const valorDaParcela = await calcularParcela(dadosDoProduto.preco, numeroParcelas)
+    console.log(`Seu produto ${produto} custa R$${dadosDoProduto.preco.toFixed(2).replace('.', ',')} e você pagará em ${numeroParcelas}x de R$${valorDaParcela.toFixed(2).replace('.', ',')}`)
+  }
+  catch(error) {
+    console.error("Capturei um erro", error)
+  }
+}
+
+resolver("hormonios", 7)
