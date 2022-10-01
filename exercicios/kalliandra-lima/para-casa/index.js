76bce210527d60643cb50ad45e89ef26061e7d90
Kalliandra Lima <kalliandralima@gmail.com>
2022-09-30 21:29:41 -0300

    Resolução S7

---
+++ exercicios/kalliandra-lima/para-casa/index.js
@@ -0,0 +1,40 @@
+function buscarPrecoDolar() {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve({
+        comercial: "5.26",
+        turismo: "5.50",
+      });
+    }, 1000);
+  });
+}
+
+function buscarJurosImportacao() {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve({
+        juros1: 0.13,
+        juros2: 0.07,
+        message:
+        "os dois juros são aplicados no valor total do produto em real",
+      });
+    }, 1000);
+  });
+}
+
+async function calcularValorEmReal(precoEmDolar) {
+  try {
+    const dolar = await buscarPrecoDolar()
+    const juros = await buscarJurosImportacao()
+    const precoEmReal = precoEmDolar * dolar.comercial
+    const precoFinal = precoEmReal + (precoEmReal * juros.juros1) + (precoEmReal * juros.juros2)
+    console.log(`O preço final do seu produto é R$ ${precoFinal.toFixed(2).replace('.',',')}`)
+  } 
+  catch (error) {
+    console.error(error)
+  }
+
+  
+}
+
+calcularValorEmReal(1270)
