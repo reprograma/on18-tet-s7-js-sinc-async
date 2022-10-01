a009cf68b3ba6fcdbf942f436197bde3b35ad575
Lilit Bandeira <lili@Lilits-Macbook-Air.local>
2022-09-30 22:11:53 -0300

    quarto commit da professora - resolução do exercício dia 30.09.22

--- exercicios/nome-aluna/para-casa/index.js
+++ exercicios/nome-aluna/para-casa/index.js
@@ -5,13 +5,14 @@ function buscarPrecoDolar() {
         comercial: 5.26,
         turismo: 5.50,
       });
-    }, 1000);
+    }, 3000);
   });
 }
 
 function buscarJurosImportacao() {
-  return new Promise((resolve) => {
+  return new Promise((resolve, reject) => {
     setTimeout(() => {
+      // return reject(new Error('BUGOU A API'))
       resolve({
         juros1: 0.13,
         juros2: 0.07,
@@ -22,7 +23,54 @@ function buscarJurosImportacao() {
   });
 }
 
+const precoEmDolar = 1270
+
+// buscarPrecoDolar()
+// .then(({comercial}) => {
+//   let precoEmReal = precoEmDolar * comercial
+//   return buscarJurosImportacao()
+//   .then(({juros1, juros2}) => {
+//     let precoFinal = precoEmReal + (precoEmReal * juros1) + (precoEmReal * juros2)
+//     console.log(`O preço final do seu produto é R$${precoFinal.toFixed(2).replace('.', ',')}`)
+//   })
+// })
+// .catch((error) => console.error("DEU ERRO: ", error))
+
 async function calcularValorEmReal(precoEmDolar) {
-  try {} 
-  catch (error) {}
+  try {
+    const dolar = await buscarPrecoDolar()
+    const precoEmReal = precoEmDolar * dolar.comercial
+    const juros = await buscarJurosImportacao()
+    const precoFinal = precoEmReal + (precoEmReal * juros.juros1) + (precoEmReal * juros.juros2)
+    console.log(`O preço final do seu produto é R$${precoFinal.toFixed(2).replace('.', ',')}`)
+  } 
+  catch (error) {
+    console.error("Capturado o erro: ", error)
+  }
 }
+
+// async function calcularValorEmReal(precoEmDolar) {
+//   try {
+//     // console.time('DURACAO')
+//     const dados = await Promise.all([
+//       buscarPrecoDolar(), 
+//       buscarJurosImportacao()
+//     ])
+//     const { comercial } = dados[0]
+//     const { juros1, juros2 } = dados[1]
+//     const precoEmReal = precoEmDolar * comercial
+//     const precoFinal = precoEmReal + (precoEmReal * juros1) + (precoEmReal * juros2)
+//     console.log(`O preço final do seu produto é R$${precoFinal.toFixed(2).replace('.', ',')}`)
+//     // console.timeEnd('DURACAO')
+//   } 
+//   catch (error) {
+//     console.error("Capturado o erro: ", error)
+//   }
+// }
+
+// calcularValorEmReal(1270)
+
+// const obj = {nome: "Leinane",
+//              turma: "on18"}
+
+// const { turma } = obj
